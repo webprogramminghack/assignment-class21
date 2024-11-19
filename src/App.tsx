42e336b44e1b04e6e2b0fa1cd85c677bc39e8cfc
RudyHarun99 <rdyhrn@gmail.com>
2024-11-19 18:09:43 +0700

    progress 1: baru mulai bikin tampilan

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import styles from './App.module.scss';
 
 const App: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      {/* header */}
+      <div className={styles.header}>
+        <p className={styles.title}>Let&apos;s Get Things Done!</p>
+        <p className={styles.description}>One Step Closer to Your Goal</p>
+      </div>
+      {/* content todo */}
+      <div className={styles.content}>
+        {/* todo form */}
+        <form className={styles.formContainer}>
+          <input
+            type="text"
+            className={styles.inputTask}
+            placeholder='Create new task'
+          />
+          <button className={styles.button}>Add</button>
+        </form>
+        {/* todo list */}
+        <div>
+          <ul>
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
