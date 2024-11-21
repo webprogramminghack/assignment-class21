aa9c2125004bbdf8010bc2b0186c003fb3be325e
RudyHarun99 <rdyhrn@gmail.com>
2024-11-21 17:46:13 +0700

    progress 3: masih tampilan, coba pakai data dummy dulu, belum component, belum global state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.scss';
 import IconTrash from './assets/svg/icon-trash.svg';
 
 const App: React.FC = () => {
+  const [ todos, setTodos ] = useState([
+    {
+      "id": "cdd88e27-fd0e-4f63-9e44-fb4bb866072e",
+      "title": "Buy groceries",
+      "completed": false,
+      "date": "2023-10-01T00:00:00.000Z"
+    },
+    {
+      "id": "ed48265e-2044-49e9-a429-b85d73b66c76",
+      "title": "Walk the dog",
+      "completed": true,
+      "date": "2023-10-02T00:00:00.000Z"
+    }
+  ]);
+
+  const [ inputNewTodo, setInputNewTodo ] = useState('');
+
   return (
     <div className={styles.container}>
       {/* header */}
@@ -18,6 +35,8 @@ const App: React.FC = () => {
             type="text"
             className={styles.inputTask}
             placeholder='Create new task'
+            value={inputNewTodo}
+            onChange={(e) => setInputNewTodo(e.target.value)}
           />
           <button className={styles.button}>
             <p>Add</p>
@@ -35,6 +54,22 @@ const App: React.FC = () => {
             <p>Learn React For 1 Hour</p>
             <IconTrash />
           </li>
+          {
+            todos.map(todo => (
+              <li
+                key={todo.id}
+                className={styles.list}
+              >
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  // onClick={(e) => setTodos([ ...todos, ])}
+                />
+                <p>{todo.title}</p>
+                <IconTrash />
+              </li>
+            ))
+          }
         </ul>
       </div>
     </div>
